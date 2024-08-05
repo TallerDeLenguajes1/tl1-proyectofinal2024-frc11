20ded82d4768e6f35db2e945ade7aa199f38f8d2
Franco <francopizzi2003@gmail.com>
2024-08-05 10:01:25 -0300

    Commit detalle 2

--- personajesJson.cs
+++ personajesJson.cs
@@ -1,6 +1,6 @@
 using System.Text.Json;
 //Persistencia de datos (Lectura y guardado de Json) 
-//Primera Parte
+//Primera Parte y segunda parte
 public class PersonajesJson{
     public void GuardarPersonajes(List<Personaje> personajes, string nombreArchivo){
         string jsonString = JsonSerializer.Serialize(personajes, new JsonSerializerOptions { WriteIndented = true });
@@ -50,52 +50,4 @@ public class PersonajesJson{
         return new List<Personaje>();
     }
 }
-}
-
-//Persistencia de datos (Lectura y guardado de Json) 
-//Segunda Parte
-public class Historial{
-    public Personaje Ganador { get; set; }
-    public string FechaPartida { get; set; }
-    public string DetallesPartida { get; set; }
-
-    public Historial(Personaje ganador, string fechaPartida, string detallesPartida){
-        Ganador = ganador;
-        FechaPartida = fechaPartida;
-        DetallesPartida = detallesPartida;
-    }
-}
-
-public class HistorialJson{
-    public void GuardarGanador(Historial historial, string nombreArchivo){
-        string jsonString = JsonSerializer.Serialize(historial, new JsonSerializerOptions { WriteIndented = true });
-        if (!File.Exists(nombreArchivo)){
-            File.WriteAllText(nombreArchivo, "[" + Environment.NewLine + jsonString + Environment.NewLine + "]");
-        }
-        else{
-            string currentContent = File.ReadAllText(nombreArchivo);
-            currentContent = currentContent.TrimEnd(']');
-            if (currentContent.Length > 1){
-                currentContent += "," + Environment.NewLine;
-            }
-            currentContent += jsonString + Environment.NewLine + "]";
-            File.WriteAllText(nombreArchivo, currentContent);
-        }
-        Console.WriteLine("Información guardada en " + nombreArchivo);
-    }
-
-    public List<Historial> LeerGanadores(string nombreArchivo){
-        if (File.Exists(nombreArchivo)){
-            string jsonString = File.ReadAllText(nombreArchivo);
-            return JsonSerializer.Deserialize<List<Historial>>(jsonString);
-        }
-        else{
-            Console.WriteLine("Archivo no encontrado.");
-            return new List<Historial>();
-        }
-    }
-
-    public bool Existe(string nombreArchivo){
-        return File.Exists(nombreArchivo) && new FileInfo(nombreArchivo).Length > 0;
-    }
 }
